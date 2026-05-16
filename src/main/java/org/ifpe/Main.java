6eb81f2556030105f65a94e6c246915b2a72bb0e
Pedro Lira <pedrovinilira2005@gmail.com>
2026-05-16 18:06:03 -0300

    chore(imports): removing unecessary imports from main class

--- src/main/java/org/ifpe/Main.java
+++ src/main/java/org/ifpe/Main.java
@@ -2,13 +2,6 @@ package org.ifpe;
 
 import org.ifpe.benchmark.BenchmarkRunner;
 import org.ifpe.client.PokeApiClient;
-import org.ifpe.services.ParallelCompletableFutureService;
-import org.ifpe.services.ParallelExecutorService;
-import org.ifpe.services.ParallelThreadService;
-import org.ifpe.services.SequentialService;
-
-import java.io.IOException;
-import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws Exception {
